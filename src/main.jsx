18adbef09172d72ac2f06c1f4e2b5a9fe1223bff
Khyati Gupta <khyatig0206@gmail.com>
2024-11-02 20:28:13 +0530

    4

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google';
-const clientId = "662912288208-bcrl7sp1r4qri1gc03bkt5ekcse1p40v.apps.googleusercontent.com";
+
+
 
 createRoot(document.getElementById('root')).render(
   
         <StrictMode>
         <BrowserRouter>
-        <GoogleOAuthProvider clientId={clientId}>
             <App />
-            </GoogleOAuthProvider>
         </BrowserRouter>
     </StrictMode>
     ,
