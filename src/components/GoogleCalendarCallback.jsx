ac455e31e281da5b715f1160cd76a094464a6341
Khyati Gupta <khyatig0206@gmail.com>
2024-11-02 21:49:43 +0530

    more

--- src/components/GoogleCalendarCallback.jsx
+++ src/components/GoogleCalendarCallback.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect ,useState} from 'react';
 import axios from 'axios';
 
 const GoogleCalendarCallback = () => {
+    const[loading,setLoading]=useState(true);
+
     useEffect(() => {
         const fetchLoginStatus = async () => {
             const urlParams = new URLSearchParams(window.location.search);
@@ -11,19 +13,29 @@ const GoogleCalendarCallback = () => {
                 try {
                     const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/auth/google/callback/?code=${code}`);
                     if (response.status === 200) {
+                        window.localStorage.setItem('access_token', response.data.access_token);
                         window.alert("Logged In Successfully");
                         window.location.href = "/";
                     }
                 } catch (error) {
                     console.error("Error during login:", error.response ? error.response.data : error.message);
                     window.alert("Failed to log in: " + (error.response ? error.response.data.message : error.message));
+                } finally {
+                    setLoading(false);
                 }
             }
         };
         fetchLoginStatus();
     }, []);
 
-    return <div>Loading...</div>;
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            {loading ? (
+                <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-mycolor"></div>
+            ) : (
+                <div>Login Complete</div>
+            )}
+        </div>
+    );
 };
-
 export default GoogleCalendarCallback;
