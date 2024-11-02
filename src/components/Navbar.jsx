75e9b03a09321ce6e11c21a12bc3380e88d11fc1
Khyati Gupta <khyatig0206@gmail.com>
2024-11-02 23:46:59 +0530

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Logout from './Logoutbutton'
+import Logoutbutton from './Logoutbutton';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
