9782ec38199e2d015c7c773e7bc12132c20e3574
Khyati Gupta <khyatig0206@gmail.com>
2024-11-03 11:09:21 +0530

    final1

---
+++ src/components/BlogCard.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const BlogCard = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_BASE_URL}/blogposts/`)
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching the blog posts:', error);
+      });
+  }, []);
+
+  return (
+    <div className="grid bg-buttoncolor2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      {posts.map(post => (
+        <div 
+        key={post.id} 
+        className="max-w-xs mx-auto bg-white shadow-md rounded-md overflow-hidden transition-transform transform cursor-pointer"
+        style={{ transitionDuration: '200ms' }}
+        onMouseEnter={(e) => e.currentTarget.style.transform = 'scale(0.98)'}
+        onMouseLeave={(e) => e.currentTarget.style.transform = 'scale(1)'}
+      >
+          <img 
+            src={`${import.meta.env.VITE_BASE_URL}${post.image}`} 
+            alt={post.title} 
+            className="w-full h-40 object-cover"
+          />
+          <div className="p-2">
+            <h2 className="text-sm font-semibold truncate">{post.title}</h2>
+            <p className="text-xs text-gray-500">By {post.author_name}</p>
+            <p className="text-xs text-gray-500">Categories: {post.categories.join(', ')}</p>
+            <p className="text-xs text-gray-800">{post.truncated_summary}</p>
+            <p className="text-xs text-gray-400">{new Date(post.created_at).toLocaleDateString()}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogCard;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import TypingEffect from 'react-typing-effect';
 import homeSvg from '/home1.svg';
+import BlogCard from '../components/BlogCard'
 
 const Homepage = () => {
   return (
+    <>
     <div className="flex flex-col-reverse lg:flex-row items-center justify-between px-8 mt-4">
       <div className="text-center w-full lg:w-1/2 sm:items-centre p-8">
         <h2 className="md:text-8xl sm:text-6xl font-bold text-textcolor maintext mb-4">Welcome to Medi</h2>
@@ -20,10 +22,12 @@ const Homepage = () => {
         </button>
         </div>
       </div>
-      <div className="w-full lg:w-1/2 mt-8">
+      <div className="w-full lg:w-1/2 mt-8 mb-3">
         <img src={homeSvg} alt="Home illustration" className="w-full h-auto border-bottom" />
       </div>
     </div>
+    <BlogCard/>
+    </>
   );
 };
 
